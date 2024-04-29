3402551d06ee65b38b27819472322980b4953792
kwon416 <bbbggg111@naver.com>
2024-04-30 01:28:00 +0900

    Feat: 메인 퍼블리싱 진행중

---
+++ components/icons/IconSearch.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+    width="17"
+    height="17"
+    viewBox="0 0 17 17"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g
+      id="&#234;&#178;&#128;&#236;&#131;&#137;_&#236;&#155;&#144;&#235;&#179;&#184;"
+    >
+      <path
+        id="Vector"
+        d="M12.75 12.75C15.0972 10.4028 15.0972 6.59721 12.75 4.25C10.4028 1.90279 6.59721 1.90279 4.25 4.25C1.90279 6.59721 1.90279 10.4028 4.25 12.75C6.59721 15.0972 10.4028 15.0972 12.75 12.75Z"
+        stroke="#939393"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+      />
+      <path
+        id="Vector_2"
+        d="M12.75 12.75L16.15 16.15"
+        stroke="#939393"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+      />
+    </g>
+  </svg>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,91 @@
 <template>
-  <h1>홈페이지</h1>
+  <v-main>
+    <v-app-bar class="px-6">
+      <IconLogo />
+      <p class="title-t25-main ps-2">DAN GEEK</p>
+
+      <v-spacer></v-spacer>
+      <IconNotification />
+      <IconMy class="ms-5" />
+    </v-app-bar>
+
+    <v-container>
+      <v-text-field rounded="30" label="궁금한 것을 적어보세요!">
+        <template v-slot:prepend-inner>
+          <IconSearch />
+        </template>
+      </v-text-field>
+
+      <div
+        class="d-flex title-t18-bold"
+        style="justify-content: center; margin-top: 40px; color: #939393"
+      >
+        <p
+          :class="windowNumber === 0 ? 'active' : ''"
+          @click="windowNumber = 0"
+        >
+          룸메찾기
+        </p>
+        <p
+          :class="windowNumber === 1 ? 'active' : ''"
+          @click="windowNumber = 1"
+        >
+          공동구매
+        </p>
+      </div>
+
+      <v-window v-model="windowNumber">
+        <!-- 1. 룸메 찾기 -->
+        <v-window-item>
+          <v-card
+            :border="true"
+            rounded="15"
+            class="justify-center align-center"
+            height="245px"
+          >
+            <template v-slot:title> 가장 최근 글 Top 3 </template>
+            <template v-slot:text>
+              <span>window {{ windowNumber + 1 }}</span>
+            </template>
+          </v-card>
+        </v-window-item>
+        <!-- 2. 공동 구매 -->
+        <v-window-item>
+          <v-card
+            :border="true"
+            rounded="15"
+            class="justify-center align-center"
+            height="245px"
+          >
+            <template v-slot:title> 가장 최근 글 Top 3 </template>
+            <template v-slot:text>
+              <span>window {{ windowNumber + 1 }}</span>
+            </template>
+          </v-card>
+        </v-window-item>
+      </v-window>
+    </v-container>
+  </v-main>
+
   <v-card title="login" @click="router.push('/login')"> </v-card>
 </template>
 
 <script setup>
 const router = useRouter();
-const ui = {
-  background: "bg-white dark:bg-slate-900",
-};
+const windowNumber = ref(0);
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.v-label {
+  color: #939393;
+  font-family: Pretendard;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+}
+
+.active {
+  color: #3867e7;
+}
+</style>
