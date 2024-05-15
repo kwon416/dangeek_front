3c0208d2b7d148995dbea2598935c4d97ef7dc58
kwon416 <bbbggg111@naver.com>
2024-05-15 22:47:17 +0900

    Feat: 공동구매 페이지 추가

--- pages/group/index.vue
+++ pages/group/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <v-app-bar class="px-6">
+    <IconMoney />
+    <div style="height: 60px; margin-left: -2px; margin-top: auto">
+      <p class="title">
+        <span style="color: #79a1e6; font-size: 20px">name</span> 님!
+      </p>
+      <p class="title">공동구매로 돈을 절약해보세요.</p>
+    </div>
+
+    <v-spacer></v-spacer>
+    <IconNotification @click="router.push('/notification')" />
+    <IconMy class="ms-5" @click="router.push('/my')" />
+  </v-app-bar>
 </template>
 
 <script setup>
@@ -10,4 +22,13 @@ definePageMeta({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.title {
+  color: #121212;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 30px; /* 166.667% */
+}
+</style>
