ae672b205110bc7b3aadc8309e2dad4d951f1a2f
kwon416 <bbbggg111@naver.com>
2024-05-20 10:58:29 +0900

    Feat: 마이 페이지 퍼블리싱

---
+++ components/icons/my/IconDocument.vue
@@ -0,0 +1,66 @@
+<template>
+  <svg
+    width="21"
+    height="22"
+    viewBox="0 0 21 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="&#234;&#184;&#128;_&#236;&#155;&#144;&#235;&#179;&#184;">
+      <path
+        id="Rectangle 639"
+        d="M0 4C0 1.79086 1.79086 0 4 0H14C17.866 0 21 3.13401 21 7V18C21 20.2091 19.2091 22 17 22H4C1.79086 22 0 20.2091 0 18V4Z"
+        fill="url(#paint0_linear_336_1603)"
+      />
+      <path
+        id="Rectangle 640"
+        d="M13.2632 0C17.5361 0 21 3.4639 21 7.73684V8.8H16.2632C14.6063 8.8 13.2632 7.45685 13.2632 5.8V0Z"
+        fill="url(#paint1_linear_336_1603)"
+      />
+      <path
+        id="Vector 65"
+        d="M4.42102 7.69995H8.84207"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Vector 66"
+        d="M4.42102 12.1H13.2631"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <linearGradient
+        id="paint0_linear_336_1603"
+        x1="7.5"
+        y1="2.84477e-08"
+        x2="14.5"
+        y2="22"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#79A1E6" />
+        <stop offset="1" stop-color="#2A5FC5" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_336_1603"
+        x1="19.5494"
+        y1="1.46667"
+        x2="15.1361"
+        y2="7.28683"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#79A1E6" />
+        <stop offset="1" stop-color="#2A5FC5" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- pages/my/index.vue
+++ pages/my/index.vue
@@ -3,19 +3,132 @@
     <TopBar title="마이페이지" />
     <v-container>
       <IconAvatar1 :width="88" :height="88" style="margin: 0 auto" />
-      <div style="justify-content: center; align-items: center; display: flex">
+      <div class="center">
         <div style="width: 15px"></div>
         <span class="title-t20-bold mx-2">name</span>
         <v-icon icon="mdi-pencil" color="#939393" size="15"></v-icon>
       </div>
-      <div style="justify-content: center; align-items: center; display: flex">
+      <div class="center">
         <p class="title-t16-medium" style="color: #939393">email</p>
       </div>
-      <v-btn style="margin: 20px auto">자기소개 작성 및 수정</v-btn>
+      <div class="center">
+        <v-btn color="#F2F4F6" style="margin: 20px auto">
+          <p class="title-t14-medium" style="color: #595959">
+            자기소개 작성 및 수정
+          </p>
+        </v-btn>
+      </div>
     </v-container>
+    <v-divider class="border-opacity-100" color="#E8E8E8" thickness="5" />
+    <v-list>
+      <v-list-item class="px-6">
+        <v-list-item-title class="title-t18-bold">
+          자기소개 작성 여부
+        </v-list-item-title>
+        <template v-slot:prepend>
+          <IconDocument class="me-4" />
+        </template>
+        <template v-slot:append>
+          <p>미완료</p>
+        </template>
+      </v-list-item>
+      <v-divider
+        class="border-opacity-100 my-2"
+        color="#E8E8E8"
+        thickness="2"
+      />
+      <v-list-item class="px-6">
+        <v-list-item-title class="title-t18-bold">
+          룸메 추천 노출 여부
+        </v-list-item-title>
+        <v-list-item-subtilte>
+          <p class="title-t11-regular-grey">
+            허용 시 다른 유저의 추천에 노출되게 됩니다.
+          </p>
+        </v-list-item-subtilte>
+        <template v-slot:prepend>
+          <IconMainRoommateActive class="me-4" />
+        </template>
+        <template v-slot:append>
+          <label class="form-switch">
+            <input type="checkbox" />
+            <i class="me-0"></i>
+          </label>
+        </template>
+      </v-list-item>
+      <v-divider
+        class="border-opacity-100 my-2"
+        color="#E8E8E8"
+        thickness="2"
+      />
+    </v-list>
   </v-main>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.center {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.form-switch {
+  display: inline-block;
+  cursor: pointer;
+  -webkit-tap-highlight-color: transparent;
+}
+.form-switch i {
+  position: relative;
+  display: inline-block;
+  margin-right: 0.5rem;
+  width: 46px;
+  height: 26px;
+  background-color: #e6e6e6;
+  border-radius: 23px;
+  vertical-align: text-bottom;
+  transition: all 0.3s linear;
+}
+.form-switch i::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  width: 42px;
+  height: 22px;
+  background-color: #fff;
+  border-radius: 11px;
+  transform: translate3d(2px, 2px, 0) scale3d(1, 1, 1);
+  transition: all 0.25s linear;
+}
+.form-switch i::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  width: 22px;
+  height: 22px;
+  background-color: #fff;
+  border-radius: 11px;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.24);
+  transform: translate3d(2px, 2px, 0);
+  transition: all 0.2s ease-in-out;
+}
+.form-switch:active i::after {
+  width: 28px;
+  transform: translate3d(2px, 2px, 0);
+}
+.form-switch:active input:checked + i::after {
+  transform: translate3d(16px, 2px, 0);
+}
+.form-switch input {
+  display: none;
+}
+.form-switch input:checked + i {
+  background-color: #3867e7;
+}
+.form-switch input:checked + i::before {
+  transform: translate3d(18px, 2px, 0) scale3d(0, 0, 0);
+}
+.form-switch input:checked + i::after {
+  transform: translate3d(22px, 2px, 0);
+}
+</style>
