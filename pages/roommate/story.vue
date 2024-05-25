24515b84cd3f8e22c9f99678e1c78b6f5085860e
kwon416 <bbbggg111@naver.com>
2024-05-26 04:32:02 +0900

    Feat: 룸메 스토리 추가

--- pages/roommate/story.vue
+++ pages/roommate/story.vue
@@ -23,7 +23,7 @@
     </v-app-bar>
     <v-container>
       <v-card
-        class="px-6 mb-2 py-5 mx-auto"
+        class="px-6 mb-4 py-5 mx-auto"
         rounded="20"
         style="justify-content: center; display: flex"
       >
@@ -66,6 +66,77 @@
           </div>
         </div>
       </v-card>
+      <v-card
+        class="px-10 mb-4 py-5 mx-auto"
+        rounded="20"
+        style="justify-content: center; display: flex"
+      >
+        <div style="display: block; width: fit-content">
+          <div class="d-flex" style="justify-content: center">
+            <p class="title" style="line-height: 15px; padding-bottom: 6px">
+              생활 습관 및 성향
+            </p>
+          </div>
+          <div class="d-flex pt-1" style="justify-content: start">
+            <p
+              class="title-t18-bold"
+              style="line-height: 15px; padding-bottom: 6px"
+            >
+              <span
+                >흡연자이며 외항형입니다. 잠버릇이 있어 같이 사는 룸메이트가
+                예민하지 않은 사람이었으면 좋겠습니다.</span
+              >
+            </p>
+          </div>
+        </div>
+      </v-card>
+      <v-card
+        class="px-10 mb-4 py-5 mx-auto"
+        rounded="20"
+        style="justify-content: center; display: flex"
+      >
+        <div style="display: block; width: fit-content">
+          <div class="d-flex" style="justify-content: center">
+            <p class="title" style="line-height: 15px; padding-bottom: 6px">
+              취미 및 관심사
+            </p>
+          </div>
+          <div class="d-flex pt-1" style="justify-content: start">
+            <p
+              class="title-t18-bold"
+              style="line-height: 15px; padding-bottom: 6px"
+            >
+              <span
+                >EPL 경기 관람하는 것을 좋아하고 직접 축구하는 것도 좋아합니다.
+              </span>
+            </p>
+          </div>
+        </div>
+      </v-card>
+      <v-card
+        class="px-10 mb-4 py-5 mx-auto"
+        rounded="20"
+        style="justify-content: center; display: flex"
+      >
+        <div style="display: block; width: fit-content">
+          <div class="d-flex" style="justify-content: center">
+            <p class="title" style="line-height: 15px; padding-bottom: 6px">
+              기대하는 룸메이트 상
+            </p>
+          </div>
+          <div class="d-flex pt-1" style="justify-content: start">
+            <p
+              class="title-t18-bold"
+              style="line-height: 15px; padding-bottom: 6px"
+            >
+              <span
+                >EPL 경기 관람하는 것을 좋아하고 직접 축구하는 것도 좋아합니다.
+              </span>
+            </p>
+          </div>
+        </div>
+      </v-card>
+      <div style="height: 93px"></div>
     </v-container>
     <div class="pb-10 bottom-btn-wrapper">
       <v-btn block class="v-btn__gradient" rounded="15" height="53">
@@ -80,6 +151,15 @@ const router = useRouter();
 </script>
 
 <style lang="scss" scoped>
+span {
+  color: #121212;
+  text-align: justify;
+  font-family: Pretendard;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 500;
+}
+
 .bottom-btn-wrapper {
   width: 100%;
   padding: 0 24px;
@@ -88,5 +168,14 @@ const router = useRouter();
   bottom: 0;
   display: flex;
   justify-content: center;
+}
+.title {
+  color: #79a1e6;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 20px; /* 142.857% */
 }
 </style>
